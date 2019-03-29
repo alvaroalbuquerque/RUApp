015f4f51c329a35926f9c29d2da9fb76d5b4c4bb
Augusto <jass2@ic.ufal.br>
2019-03-29 10:54:07 -0300

    Bug corrigido slider

--- app/src/main/java/com/example/ruapp/SliderAdapter.java
+++ app/src/main/java/com/example/ruapp/SliderAdapter.java
@@ -4,10 +4,13 @@ import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
 import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.zip.Inflater;
 
@@ -26,40 +29,39 @@ public class SliderAdapter extends PagerAdapter {
 
     @Override
     public int getCount() {
-        //return number of slides
         return slide_titles.length;
     }
     @Override
     public boolean isViewFromObject(@NonNull View view, @NonNull Object o) {
-        return view == (ConstraintLayout) o;
+        return view == o;
     }
 
     @NonNull
     @Override
     public Object instantiateItem(@NonNull ViewGroup container, int position) {
         layoutInflater = (LayoutInflater)context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
-        View view = layoutInflater.inflate(R.layout.activity_see_menu_slide_layout,container,false);
+        View view = layoutInflater.inflate(R.layout.activity_see_menu_slide_layout, null);
 
         TextView slideTitleText = view.findViewById(R.id.slideTitleTextID);
         TextView textFirstView = view.findViewById(R.id.textFirstViewID);
         TextView textSecondView = view.findViewById(R.id.textSecondViewID);
         TextView textThirdView = view.findViewById(R.id.textThirdViewID);
-/*
+
         slideTitleText.setText(slide_titles[position]); //position é uma variavel do pager adapter
-        textFirstView.setText(slide_subtitles1[position]);
-        textSecondView.setText(slide_subtitles2[position]);
-        textThirdView.setText(slide_subtitles3[position]);*/
-        slideTitleText.setText("AAAAA"); //position é uma variavel do pager adapter
         textFirstView.setText(slide_subtitles1[position]);
         textSecondView.setText(slide_subtitles2[position]);
         textThirdView.setText(slide_subtitles3[position]);
 
+        ViewPager vp = (ViewPager) container;
+        vp.addView(view, 0);
         return view;
 
     }
 
     @Override
     public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
-        container.removeView((ConstraintLayout)object);
+        ViewPager vp = (ViewPager) container;
+        View view = (View) object;
+        vp.removeView(view);
     }
 }
